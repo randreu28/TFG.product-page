59e646b11f8209caab5f0856c52e6becc0f8e708
Rubén Chiquin <randreu28@gmail.com>
2022-12-26 22:19:50 +0100

    Adds a headphone model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Headphones } from "./components/Headphones";
+
 export default function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">App</div>
+      <div className="flex items-center justify-center h-screen">
+        <Canvas>
+          <pointLight position={[0, 20, 10]} intensity={1.5} />
+          <OrbitControls />
+          <Headphones />
+        </Canvas>
+      </div>
     </>
   );
 }
