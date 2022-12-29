2aa60f887d25e9cf10af787fd019383ef35f32f2
Rubén Chiquin <randreu28@gmail.com>
2022-12-29 21:34:53 +0100

    Creates a layout for the product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Model } from "./components/Model";
+import MyScene from "./components/MyScene";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
-        <Canvas>
-          <ambientLight intensity={1} />
-          <pointLight position={[0, 0, 0]} intensity={0.5} />
+      <Navbar />
+      <div className="grid grid-cols-1 lg:grid-cols-3 max-w-[100rem] m-auto">
+        <div className="h-[80vh] m-5 col-span-2">
+          <Canvas className="rounded-xl">
+            <MyScene />
+          </Canvas>
+        </div>
+        <div className="p-5 bg-gray-800 rounded-xl m-5 flex flex-col justify-between gap-10">
+          <div className="space-y-10">
+            <div className="space-y-2">
+              <h2 className="font-bold text-3xl">Cool headphones</h2>
+              <div className="flex space-x-2">
+                <p className="text-lg">$353.95</p>
+                <p className="bg-gray-700 rounded-full px-2 text-sm my-auto">
+                  USD
+                </p>
+              </div>
+            </div>
+            <div className="space-y-2">
+              <h3 className="font-bold text-xl">Color</h3>
+              <div className="flex space-x-3 justify-center sm:justify-start pt-5">
+                <div className="bg-blue-600 rounded-full w-10 h-10 border-8 border-gray-700" />
+                <div className="bg-red-600 rounded-full w-10 h-10 border-8 border-gray-700" />
+                <div className="bg-green-600 rounded-full w-10 h-10 border-8 border-gray-700" />
+              </div>
+            </div>
+            <div className="space-y-2">
+              <h3 className="font-bold text-xl">Variant</h3>
+              <div className="flex flex-col gap-5 w-fit text-center sm:flex-row mx-auto sm:mx-0 pt-5">
+                <abbr
+                  title="Chargable via USB-C"
+                  className="bg-gray-700 rounded-full px-3 py-1  my-auto no-underline line-clamp-1"
+                >
+                  Wired
+                </abbr>
+                <abbr
+                  title="Up to 30 battery hours"
+                  className="bg-gray-700 rounded-full px-3 py-1  my-auto no-underline line-clamp-1"
+                >
+                  Wireless
+                </abbr>
+                <abbr
+                  title="Up to 100 battery hours"
+                  className="bg-gray-700 rounded-full px-3 py-1  my-auto no-underline line-clamp-1"
+                >
+                  Wireless pro
+                </abbr>
+              </div>
+            </div>
+            <p className="leading-loose text-gray-400 line-clamp-5">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur esse quam cupiditate commodi dicta soluta aliquid.
+              libero velit quisquam? Eos cum, accusantium quam consectetur
+              officiis suscipit aut aperiam voluptatibus quos.
+            </p>
+          </div>
 
-          <pointLight position={[5, 0, 0]} intensity={0.25} />
-          <pointLight position={[0, 5, 0]} intensity={0.25} />
-          <pointLight position={[0, 0, 5]} intensity={0.25} />
-          <pointLight position={[-5, 0, 0]} intensity={0.25} />
-          <pointLight position={[0, -5, 0]} intensity={0.25} />
-          <pointLight position={[0, 0, -5]} intensity={0.25} />
-
-          <OrbitControls />
-          <Model />
-        </Canvas>
+          <button className="w-full rounded-xl text-center py-5 bg-gray-700">
+            Add to cart
+          </button>
+        </div>
       </div>
     </>
   );
---
+++ src/components/MyScene.tsx
@@ -0,0 +1,36 @@
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { Model } from "./Model";
+import { useState } from "react";
+import { Euler } from "three";
+
+export default function MyScene() {
+  const zoom: number = window.innerWidth < 640 ? 6 : 4;
+
+  const [modelRotation, setModelRotation] = useState<Euler>(new Euler(0, 0, 0));
+
+  useFrame(() => {
+    const newRotation = modelRotation.clone();
+    newRotation.y += 0.001;
+    setModelRotation(newRotation);
+  });
+  return (
+    <>
+      <color attach="background" args={["rgb(31, 41, 55)"]} />
+
+      <ambientLight intensity={1} />
+      <pointLight position={[0, 0, 0]} intensity={0.5} />
+      <pointLight position={[5, 0, 0]} intensity={0.25} />
+      <pointLight position={[0, 5, 0]} intensity={0.25} />
+      <pointLight position={[0, 0, 5]} intensity={0.25} />
+      <pointLight position={[-5, 0, 0]} intensity={0.25} />
+      <pointLight position={[0, -5, 0]} intensity={0.25} />
+      <pointLight position={[0, 0, -5]} intensity={0.25} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, zoom]} />
+      <OrbitControls enablePan={false} rotateSpeed={0.3} />
+
+      <Model rotation={modelRotation} />
+    </>
+  );
+}
