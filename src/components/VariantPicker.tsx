e5ccc40791f3d769f79405ae2ef0e1785c109230
Rubén Chiquin <randreu28@gmail.com>
2023-01-07 17:38:22 +0100

    Changes variant to RGB based

--- src/components/VariantPicker.tsx
+++ src/components/VariantPicker.tsx
@@ -6,40 +6,36 @@ export default function VariantPicker() {
     <>
       <abbr
         onClick={() => {
-          specs.setVariant("wired");
+          specs.setVariant("no RGB");
         }}
-        title="Chargable via USB-C"
+        title="No RGB leds"
         className={`rounded-full px-3 py-1 my-auto no-underline line-clamp-1 hover:cursor-pointer transition-colors duration-300 border ${
-          specs.variant == "wired" ? "border-teal-500" : "border-pink-500/25"
+          specs.variant == "no RGB" ? "border-teal-500" : "border-pink-500/25"
         }`}
       >
-        Wired
+        no RGB
       </abbr>
       <abbr
         onClick={() => {
-          specs.setVariant("wireless");
+          specs.setVariant("RGB");
         }}
-        title="Up to 30 battery hours"
+        title="With red lights only"
         className={`rounded-full px-3 py-1 my-auto no-underline line-clamp-1 hover:cursor-pointer transition-colors duration-300 border ${
-          specs.variant == "wireless"
-            ? "border-teal-500"
-            : " border-pink-500/25"
+          specs.variant == "RGB" ? "border-teal-500" : " border-pink-500/25"
         }`}
       >
-        Wireless
+        RGB
       </abbr>
       <abbr
         onClick={() => {
-          specs.setVariant("wireless pro");
+          specs.setVariant("RGB pro");
         }}
-        title="Up to 100 battery hours"
+        title="Up to 16k different colors"
         className={`rounded-full px-3 py-1 my-auto no-underline line-clamp-1 hover:cursor-pointer transition-colors duration-300 border ${
-          specs.variant == "wireless pro"
-            ? "border-teal-500"
-            : "border-pink-500/25"
+          specs.variant == "RGB pro" ? "border-teal-500" : "border-pink-500/25"
         }`}
       >
-        Wireless pro
+        RGB pro
       </abbr>
     </>
   );
--- src/specStore.ts
+++ src/specStore.ts
@@ -2,14 +2,14 @@ import create from "zustand";
 
 export type Specs = {
   color: "black" | "white" | "blue";
-  variant: "wired" | "wireless" | "wireless pro";
+  variant: "no RGB" | "RGB" | "RGB pro";
   setColor: (newColor: Specs["color"]) => void;
   setVariant: (newVariant: Specs["variant"]) => void;
 };
 
 export const useSpecStore = create<Specs>((set) => ({
   color: "black",
-  variant: "wired",
+  variant: "RGB",
   setColor: (newColor) => set(() => ({ color: newColor })),
   setVariant: (newVariant) => set(() => ({ variant: newVariant })),
 }));
