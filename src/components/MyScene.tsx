8cefcf80a705b10be2d0666c961534fd9d8344b2
Rubén Chiquin <randreu28@gmail.com>
2023-01-07 17:34:07 +0100

    Tweaks scene light values

--- src/components/MyScene.tsx
+++ src/components/MyScene.tsx
@@ -6,7 +6,7 @@ import { Model } from "./Model";
 
 export default function MyScene() {
   const zoom: number = window.innerWidth < 640 ? 6 : 4;
-
+  const lightIntensity: number = 0.3;
   const [modelRotation, setModelRotation] = useState<Euler>(new Euler(0, 0, 0));
 
   useFrame(() => {
@@ -16,14 +16,14 @@ export default function MyScene() {
   });
   return (
     <>
-      <ambientLight intensity={1} />
-      <pointLight position={[0, 0, 0]} intensity={0.5} />
-      <pointLight position={[5, 0, 0]} intensity={0.25} />
-      <pointLight position={[0, 5, 0]} intensity={0.25} />
-      <pointLight position={[0, 0, 5]} intensity={0.25} />
-      <pointLight position={[-5, 0, 0]} intensity={0.25} />
-      <pointLight position={[0, -5, 0]} intensity={0.25} />
-      <pointLight position={[0, 0, -5]} intensity={0.25} />
+      <ambientLight intensity={lightIntensity} />
+      <pointLight position={[0, 0, 0]} intensity={lightIntensity} />
+      <pointLight position={[5, 0, 0]} intensity={lightIntensity} />
+      <pointLight position={[0, 5, 0]} intensity={lightIntensity} />
+      <pointLight position={[0, 0, 5]} intensity={lightIntensity} />
+      <pointLight position={[-5, 0, 0]} intensity={lightIntensity} />
+      <pointLight position={[0, -5, 0]} intensity={lightIntensity} />
+      <pointLight position={[0, 0, -5]} intensity={lightIntensity} />
 
       <PerspectiveCamera makeDefault position={[0, 0, zoom]} />
       <OrbitControls enablePan={false} rotateSpeed={0.3} />
